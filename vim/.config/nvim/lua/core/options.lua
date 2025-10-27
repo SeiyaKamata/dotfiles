e44d8789db871131304ccbf022ae57a20dbf731c
SeiyaKamata <kmt2000200@gmail.com>
2025-10-27 23:20:38 +0900

    feat: vim設定

---
+++ vim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,28 @@
+-- 基本オプション
+local opt = vim.opt
+
+-- 表示系
+opt.number = true           -- 行番号を表示
+opt.relativenumber = true   -- 相対行番号
+opt.cursorline = true       -- カーソル行をハイライト
+opt.termguicolors = true    -- 24bitカラー有効化
+opt.signcolumn = "yes"      -- 常にサインカラムを表示
+opt.wrap = false            -- 折り返し無効化
+
+-- インデント系
+opt.expandtab = true        -- タブをスペースに変換
+opt.shiftwidth = 2          -- 自動インデントの幅
+opt.tabstop = 2             -- タブ幅
+opt.smartindent = true      -- スマートインデント
+
+-- 検索系
+opt.ignorecase = true       -- 大文字小文字を無視
+opt.smartcase = true        -- 大文字が含まれていたら区別
+opt.incsearch = true        -- インクリメンタルサーチ
+opt.hlsearch = true         -- 検索結果をハイライト
+
+-- その他
+opt.clipboard = "unnamedplus" -- システムクリップボード連携
+opt.scrolloff = 8             -- 上下に余白を持たせる
+opt.splitbelow = true         -- 横分割は下に開く
+opt.splitright = true         -- 縦分割は右に開く
---
+++ vim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
+    config = function()
+      require("mason").setup()
+
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "html",
+          "cssls",
+          "ts_ls",
+          "gopls",
+          "pyright",
+          "ruby_lsp",
+          "clangd",
+        },
+        handlers = {
+          -- デフォルト設定（全LSP共通）
+          function(server_name)
+            vim.lsp.config[server_name].setup({})
+          end,
+
+          -- TypeScript / JavaScript
+          ["ts_ls"] = function()
+            vim.lsp.config.ts_ls.setup({
+              on_attach = function(client)
+                client.server_capabilities.documentFormattingProvider = false
+              end,
+            })
+          end,
+
+          -- Go
+          ["gopls"] = function()
+            vim.lsp.config.gopls.setup({
+              on_attach = function()
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                  pattern = "*.go",
+                  callback = function()
+                    vim.lsp.buf.format({ async = false })
+                  end,
+                })
+              end,
+            })
+          end,
+
+          -- Python
+          ["pyright"] = function()
+            vim.lsp.config.pyright.setup({
+              settings = {
+                python = {
+                  analysis = { typeCheckingMode = "basic" },
+                },
+              },
+            })
+          end,
+
+          -- C / C++
+          ["clangd"] = function()
+            vim.lsp.config.clangd.setup({
+              cmd = { "clangd", "--background-index" },
+              filetypes = { "c", "cpp", "objc", "objcpp" },
+            })
+          end,
+        },
+      })
+    end,
+  },
+}
