9e1a117348562e097b54db420dc9640a85098461
SeiyaKamata <kmt2000200@gmail.com>
2025-11-05 08:40:31 +0900

    rustコマンド追加

--- zsh/.zsh/aliases/base-alias.zsh
+++ zsh/.zsh/aliases/base-alias.zsh
@@ -3,3 +3,15 @@ alias clip="pbcopy"
 alias vim="nvim"
 alias cdh="cd  ~/"
 alias cdb='mkdir -p "$1" && cd "$1"'
+
+
+# Rust command
+alias ls='eza'
+alias ps='procs'
+alias cat='bat'
+alias du='dust'
+alias find='fd'
+alias df='duf'
+alias top='btm'
+alias grep='rg'
+alias curl='httpie'
