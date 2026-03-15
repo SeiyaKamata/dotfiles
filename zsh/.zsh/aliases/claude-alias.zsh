c80c47794e6d1adc61065812273633293a8dff61
SeiyaKamata <kmt2000200@gmail.com>
2026-03-15 14:31:37 +0900

    feat: add claude code alias

---
+++ zsh/.zsh/aliases/claude-alias.zsh
@@ -0,0 +1,8 @@
+
+alias claude="echo 'to use claude code, you have to claudep or claudew'"
+
+# 個人用
+alias claudep="CLAUDE_CONFIG_DIR=~/.claude-p command claude"
+
+# 仕事用
+alias claudew="CLAUDE_CONFIG_DIR=~/.claude-w command claude"
