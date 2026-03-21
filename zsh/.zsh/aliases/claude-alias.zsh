73accc12553f125e509d724a9bd6f566503e2aaa
SeiyaKamata <kmt2000200@gmail.com>
2026-03-21 19:22:46 +0900

    fix: 個人用のみ定義しておく

--- zsh/.zsh/aliases/claude-alias.zsh
+++ zsh/.zsh/aliases/claude-alias.zsh
@@ -1,8 +1,3 @@
-
-alias claude="echo 'to use claude code, you have to claudep or claudew'"
-
 # 個人用
 alias claudep="CLAUDE_CONFIG_DIR=~/.claude-p command claude"
 
-# 仕事用
-alias claudew="CLAUDE_CONFIG_DIR=~/.claude-w command claude"
