8bb18efd5b90505e2db7fc08d86f2b587d845d5d
SeiyaKamata <kmt2000200@gmail.com>
2026-05-09 22:17:03 +0900

    fix: tmux keymaps修正

--- vim/.config/nvim/lua/core/keymaps.lua
+++ vim/.config/nvim/lua/core/keymaps.lua
@@ -1,54 +1,41 @@
--- キーマッピング
 local function map(mode, lhs, rhs, desc)
 	vim.keymap.set(mode, lhs, rhs, { desc = desc, noremap = true, silent = true })
 end
 
--- スペースをリーダーキーに
 vim.g.mapleader = " "
 
--- 基本操作
-map("i", "jj", "<Esc>", "jj でノーマルモード")
-map("n", "<leader>w", ":w<CR>", "保存")
-map("n", "<leader>q", ":q<CR>", "終了")
-map("n", "<leader>h", ":nohlsearch<CR>", "検索ハイライト解除")
-map("n", "<leader>rp", ":RelPath<CR>", "相対パスをコピー")
-map("n", "H", "^", "行頭")
-map("n", "L", "$", "行末")
-map("n", "J", "}", "段落を下へ")
-map("n", "K", "{", "段落を上へ")
+map("i", "jj",         "<Esc>",                                            "jj でノーマルモード")
+map("n", "<leader>x",  "<cmd>x<CR>",                                       "保存して終了")
+map("n", "<leader>h",  "<cmd>nohlsearch<CR>",                              "検索ハイライト解除")
+map("n", "<leader>rp", "<cmd>RelPath<CR>",                                 "相対パスをコピー")
+map("n", "H",          "^",                                                "行頭")
+map("n", "L",          "$",                                                "行末")
+map("n", "J",          "}",                                                "段落を下へ")
+map("n", "K",          "{",                                                "段落を上へ")
 
-map("n", "<C-j>", ":m .+1<CR>==", "現在行を下へ移動")
-map("n", "<C-k>", ":m .-2<CR>==", "現在行を上へ移動")
-map("v", "<C-j>", ":m '>+1<CR>gv=gv", "選択行を下へ移動")
-map("v", "<C-k>", ":m '<-2<CR>gv=gv", "選択行を上へ移動")
-map("i", "<C-j>", "<Esc>:m .+1<CR>==gi", "挿入モードで現在行を下へ移動")
-map("i", "<C-k>", "<Esc>:m .-2<CR>==gi", "挿入モードで現在行を上へ移動")
+map("n", "<C-j>",      "<cmd>m .+1<CR>==",                                 "現在行を下へ移動")
+map("n", "<C-k>",      "<cmd>m .-2<CR>==",                                 "現在行を上へ移動")
+map("v", "<C-j>",      "<cmd>m '>+1<CR>gv=gv",                            "選択行を下へ移動")
+map("v", "<C-k>",      "<cmd>m '<-2<CR>gv=gv",                            "選択行を上へ移動")
+map("i", "<C-j>",      "<Esc><cmd>m .+1<CR>==gi",                         "挿入モードで現在行を下へ移動")
+map("i", "<C-k>",      "<Esc><cmd>m .-2<CR>==gi",                         "挿入モードで現在行を上へ移動")
 
--- 置換
-map("n", "<leader>sr", ":%s//g<Left><Left>", "バッファ全体を置換")
-map("v", "<leader>sr", ":s//g<Left><Left>", "選択範囲を置換")
-map("n", "<leader>sw", ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>", "カーソル単語をバッファ全体で置換")
+map("n", "<leader>sr", ":%s//g<Left><Left>",                               "バッファ全体を置換")
+map("v", "<leader>sr", ":s//g<Left><Left>",                                "選択範囲を置換")
+map("n", "<leader>sw", ":%s/\\<<C-r><C-w>\\>//gI<Left><Left><Left>",      "カーソル単語をバッファ全体で置換")
 
--- buffer操作系
-map("n", "<Tab>", "<cmd>bnext<CR>", "次のバッファへ")
-map("n", "<S-Tab>", "<cmd>bprevious<CR>", "前のバッファへ")
-map("n", "<leader><leader>", "<cmd>b#<CR>", "直前のバッファへ")
-map("n", "<leader>bb", "<cmd>buffers<CR>:buffer ", "バッファ一覧から選択")
-map("n", "<leader>bd", "<cmd>bdelete<CR>", "現在のバッファを削除")
+map("n", "<Tab>",            "<cmd>bnext<CR>",           "次のバッファへ")
+map("n", "<S-Tab>",          "<cmd>bprevious<CR>",       "前のバッファへ")
+map("n", "<leader><leader>", "<cmd>b#<CR>",              "直前のバッファへ")
+map("n", "<leader>bb",       "<cmd>buffers<CR>:buffer ", "バッファ一覧から選択")
+map("n", "<leader>bd",       "<cmd>bdelete<CR>",         "現在のバッファを削除")
 
--- ウィンドウ操作
---map("n", "<leader>sv", "<C-w>v", "垂直分割")
---map("n", "<leader>sh", "<C-w>s", "水平分割")
---map("n", "<leader>se", "<C-w>=", "サイズを等分")
---map("n", "<leader>sx", ":close<CR>", "ウィンドウを閉じる")
-
--- いらないキーバインド
-map("n", "q", "<Nop>", "")
-map("n", "Q", "<Nop>", "")
+map("n", "q",  "<Nop>", "")
+map("n", "Q",  "<Nop>", "")
 map("n", "q:", "<Nop>", "")
 map("n", "q/", "<Nop>", "")
 map("n", "q?", "<Nop>", "")
 map("n", "ZZ", "<Nop>", "")
 map("n", "ZQ", "<Nop>", "")
-map("n", "?", "<Nop>", "")
-map("n", "s", "<Nop>", "")
+map("n", "?",  "<Nop>", "")
+map("n", "s",  "<Nop>", "")
