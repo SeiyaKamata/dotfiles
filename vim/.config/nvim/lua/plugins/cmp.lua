0c59e38640dd9fc3b6c3f584c49b296e1a2b7ecd
SeiyaKamata <kmt2000200@gmail.com>
2026-05-09 22:17:49 +0900

    fix: tmux cmp.lua

--- vim/.config/nvim/lua/plugins/cmp.lua
+++ vim/.config/nvim/lua/plugins/cmp.lua
@@ -9,19 +9,12 @@ return {
 	config = function()
 		local cmp = require("cmp")
 		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
 			mapping = cmp.mapping.preset.insert({
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{
-					name = "buffer",
-				},
+				{ name = "buffer" },
 				{ name = "path" },
 			}),
 		})
