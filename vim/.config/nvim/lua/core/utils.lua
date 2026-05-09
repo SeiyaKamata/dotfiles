20dbc7ce4dd0eb7a17691893a2176cc9c3e75de8
SeiyaKamata <kmt2000200@gmail.com>
2026-05-09 22:18:00 +0900

    fix: tmux mason-lspconfig

--- vim/.config/nvim/lua/core/utils.lua
+++ vim/.config/nvim/lua/core/utils.lua
@@ -1,15 +1,5 @@
 local M = {}
 
---- 汎用キーマップ
---- @param mode string | table
---- @param lhs string
---- @param rhs string | function
---- @param opts? table
-function M.map(mode, lhs, rhs, opts)
-	vim.keymap.set(mode, lhs, rhs, vim.tbl_extend("force", { noremap = true, silent = true }, opts or {}))
-end
-
---- バッファローカルなキーマップを返す関数
 --- @param bufnr integer
 --- @return function
 function M.buf_map(bufnr)
@@ -21,31 +11,6 @@ function M.buf_map(bufnr)
 			vim.tbl_extend("force", { noremap = true, silent = true, buffer = bufnr }, opts or {})
 		)
 	end
-end
-
---- 保存時に実行するautocmdを作成
---- @param pattern string
---- @param callback function
-function M.on_save(pattern, callback)
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = pattern,
-		callback = callback,
-	})
-end
-
---- LSP共通のon_attach（キーバインド設定）
---- @param _ any
---- @param bufnr integer
-function M.lsp_on_attach(_, bufnr)
-	local map = M.buf_map(bufnr)
-	map("n", "<leader>ca", vim.lsp.buf.code_action)
-	map("n", "<leader>gf", function()
-		vim.lsp.buf.format({ async = true })
-	end)
-	map("n", "<leader>rn", vim.lsp.buf.rename)
-	map("n", "<leader>gd", vim.lsp.buf.definition)
-	map("n", "<leader>gr", vim.lsp.buf.references)
-	map("n", "<leader>gk", vim.lsp.buf.hover)
 end
 
 return M
--- vim/.config/nvim/lua/plugins/mason-lspconfig.lua
+++ vim/.config/nvim/lua/plugins/mason-lspconfig.lua
@@ -2,24 +2,26 @@ return {
 	"williamboman/mason-lspconfig.nvim",
 	dependencies = { "neovim/nvim-lspconfig", "williamboman/mason.nvim" },
 	config = function()
-		local on_attach = require("core.utils").lsp_on_attach
+		local buf_map = require("core.utils").buf_map
+
+		local function on_attach(_, bufnr)
+			local map = buf_map(bufnr)
+			map("n", "<leader>ca", vim.lsp.buf.code_action,                          { desc = "Code action" })
+			map("n", "<leader>gf", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format" })
+			map("n", "<leader>rn", vim.lsp.buf.rename,                               { desc = "Rename" })
+			map("n", "<leader>gd", vim.lsp.buf.definition,                           { desc = "Go to definition" })
+			map("n", "<leader>gr", vim.lsp.buf.references,                           { desc = "References" })
+			map("n", "<leader>gk", vim.lsp.buf.hover,                                { desc = "Hover" })
+		end
 
 		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"html",
-				"cssls",
-				"ts_ls",
-				"gopls",
-				"pyright",
-				"clangd",
-			},
+			ensure_installed = { "html", "cssls", "ts_ls", "gopls", "pyright", "clangd" },
 			automatic_enable = true,
 		})
 
-		-- デフォルト設定（全LSP共通）
 		vim.lsp.config("*", { on_attach = on_attach })
 
-		-- TypeScript / JavaScript
+		-- ts_ls: formatting delegated to prettier
 		vim.lsp.config("ts_ls", {
 			on_attach = function(client, bufnr)
 				client.server_capabilities.documentFormattingProvider = false
@@ -27,7 +29,6 @@ return {
 			end,
 		})
 
-		-- Go
 		vim.lsp.config("gopls", {
 			on_attach = function(client, bufnr)
 				on_attach(client, bufnr)
@@ -35,26 +36,17 @@ return {
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					group = augroup,
 					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format({ async = false })
-					end,
+					callback = function() vim.lsp.buf.format({ async = false }) end,
 				})
 			end,
 		})
 
-		-- Python
 		vim.lsp.config("pyright", {
-			settings = {
-				python = {
-					analysis = { typeCheckingMode = "basic" },
-				},
-			},
+			settings = { python = { analysis = { typeCheckingMode = "basic" } } },
 		})
 
-		-- C / C++
 		vim.lsp.config("clangd", {
 			cmd = { "clangd", "--background-index" },
-			filetypes = { "c", "cpp", "objc", "objcpp" },
 		})
 	end,
 }
