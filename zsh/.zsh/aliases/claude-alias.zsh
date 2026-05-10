258bf39474202c32fff20bbf02d334ec241f5ec1
SeiyaKamata <kmt2000200@gmail.com>
2026-05-10 16:15:07 +0900

    fix: claude code alias修正

--- zsh/.zsh/aliases/claude-alias.zsh
+++ zsh/.zsh/aliases/claude-alias.zsh
@@ -1,3 +1,10 @@
+
+alias cc="claude"
+alias ccr="claude --resume"
+
+
 # 個人用
-alias claudep="CLAUDE_CONFIG_DIR=~/.claude-p command claude"
+alias ccp="CLAUDE_CONFIG_DIR=~/.claude-p command claude"
+alias ccpr="CLAUDE_CONFIG_DIR=~/.claude-p command claude --resume"
+
 
