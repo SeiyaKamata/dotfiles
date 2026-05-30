7d823083de5cdbf07e68a2c6857542ef357ccecb
SeiyaKamata <kmt2000200@gmail.com>
2026-05-30 20:55:07 +0900

    feat(nix-home-manager): シェル環境モジュール(zsh/sheldon/starship/zoxide)を実装

---
+++ modules/shell.nix
@@ -0,0 +1,42 @@
+{ pkgs, config, ... }: {
+  programs.zsh = {
+    enable = true;
+    dotDir = ".config/zsh";
+    envExtra = builtins.readFile ../zsh/.zshenv;
+    initExtra = ''
+      # .config/zsh/*.zsh をソース
+      for f in ${config.home.homeDirectory}/.config/zsh/*.zsh; do
+        source "$f"
+      done
+    '';
+  };
+
+  programs.sheldon = {
+    enable = true;
+    settings = {
+      shell = "zsh";
+      plugins = {
+        zsh-completions = {
+          github = "zsh-users/zsh-completions";
+        };
+        zsh-autosuggestions = {
+          github = "zsh-users/zsh-autosuggestions";
+        };
+        fast-syntax-highlighting = {
+          github = "zdharma-continuum/fast-syntax-highlighting";
+        };
+      };
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = builtins.fromTOML (builtins.readFile ../starship/.config/starship.toml);
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
